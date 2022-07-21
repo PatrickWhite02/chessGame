ddf031ad1bb4aa78d22f4cc1ddbed0913e5b1131
Patrick <patrick.white@telus.net>
2022-07-20 17:41:40 -0700

    reorganized castle movements so as to clean up my action listener

--- src/com/Board.java
+++ src/com/Board.java
@@ -57,45 +57,9 @@ public class Board extends JPanel {
             if(clicked.isGlowing()){
                 clicked.setValue(prevTile.getValueAsString());
                 prevTile.setValue("blank");
-                //If we castled the white king to the right then we need to manually move the rook. I don't have to include a rook boolean
-                if(clicked.getValue() == whiteKing && !tiles[0].getWhiteKingHasMoved() && clicked.getCoords() == 61){
-                    tiles[63].setValue("blank");
-                    tiles[60].setValue("whiteRook");
-                }
-                //If we castled the white king to the left then we need to manually move the rook. I don't have to include a rook boolean
-                if(clicked.getValue() == whiteKing && !tiles[0].getWhiteKingHasMoved() && clicked.getCoords() == 57){
-                    tiles[56].setValue("blank");
-                    tiles[58].setValue("whiteRook");
-                }
-                //If we castled the black king to the right then we need to manually move the rook. I don't have to include a rook boolean
-                if(clicked.getValue() == blackKing && !tiles[0].getBlackKingHasMoved() && clicked.getCoords() == 6){
-                    tiles[7].setValue("blank");
-                    tiles[5].setValue("blackRook");
-                }
-                //If we castled the black king to the left then we need to manually move the rook. I don't have to include a rook boolean
-                if(clicked.getValue() == blackKing && !tiles[0].getBlackKingHasMoved() && clicked.getCoords() == 2){
-                    tiles[0].setValue("blank");
-                    tiles[3].setValue("blackRook");
-                }
-                //I have to keep track if a rook or a king has moved yet for castling
-                if(clicked.getValue() == whiteKing){
-                    tiles[0].setWhiteKingHasMoved();
-                }
-                if(clicked.getValue() == blackKing){
-                    tiles[0].setBlackKingHasMoved();
-                }
-                if(clicked.getValue() == whiteRook && prevTile.getCoords() == 56){
-                    tiles[0].setWhiteRookLeftHasMoved();
-                }
-                if(clicked.getValue() == whiteRook && prevTile.getCoords() == 63){
-                    tiles[0].setWhiteRookRightHasMoved();
-                }
-                if(clicked.getValue() == blackRook && prevTile.getCoords() == 0){
-                    tiles[0].setBlackRookLeftHasMoved();
-                }
-                if(clicked.getValue() == blackRook && prevTile.getCoords() == 7){
-                    tiles[0].setBlackRookRightHasMoved();
-                }
+                checkIfCastle(clicked);
+                checkRookKingMoves(clicked);
+                checkPawnReachedEnd(clicked);
                 //swap turns
                 if(whoTurn == blackTurn){
                     whoTurn = whiteTurn;
@@ -120,6 +84,52 @@ public class Board extends JPanel {
         //store the recently clicked tile, so that we can move it on the next click
         prevTile = clicked;
     };
+    public void checkIfCastle(Tile clicked){
+        //If we castled the white king to the right then we need to manually move the rook. I don't have to include a rook boolean
+        if(clicked.getValue() == whiteKing && !tiles[0].getWhiteKingHasMoved() && clicked.getCoords() == 61){
+            tiles[63].setValue("blank");
+            tiles[60].setValue("whiteRook");
+        }
+        //If we castled the white king to the left then we need to manually move the rook. I don't have to include a rook boolean
+        if(clicked.getValue() == whiteKing && !tiles[0].getWhiteKingHasMoved() && clicked.getCoords() == 57){
+            tiles[56].setValue("blank");
+            tiles[58].setValue("whiteRook");
+        }
+        //If we castled the black king to the right then we need to manually move the rook. I don't have to include a rook boolean
+        if(clicked.getValue() == blackKing && !tiles[0].getBlackKingHasMoved() && clicked.getCoords() == 6){
+            tiles[7].setValue("blank");
+            tiles[5].setValue("blackRook");
+        }
+        //If we castled the black king to the left then we need to manually move the rook. I don't have to include a rook boolean
+        if(clicked.getValue() == blackKing && !tiles[0].getBlackKingHasMoved() && clicked.getCoords() == 2){
+            tiles[0].setValue("blank");
+            tiles[3].setValue("blackRook");
+        }
+    }
+    public void checkRookKingMoves(Tile clicked){
+        //I have to keep track if a rook or a king has moved yet for castling
+        if(clicked.getValue() == whiteKing){
+            tiles[0].setWhiteKingHasMoved();
+        }
+        if(clicked.getValue() == blackKing){
+            tiles[0].setBlackKingHasMoved();
+        }
+        if(clicked.getValue() == whiteRook && prevTile.getCoords() == 56){
+            tiles[0].setWhiteRookLeftHasMoved();
+        }
+        if(clicked.getValue() == whiteRook && prevTile.getCoords() == 63){
+            tiles[0].setWhiteRookRightHasMoved();
+        }
+        if(clicked.getValue() == blackRook && prevTile.getCoords() == 0){
+            tiles[0].setBlackRookLeftHasMoved();
+        }
+        if(clicked.getValue() == blackRook && prevTile.getCoords() == 7){
+            tiles[0].setBlackRookRightHasMoved();
+        }
+    }
+    public void checkPawnReachedEnd(Tile clicked){
+
+    }
     public void setTileValues(int i){
         tiles[i].setValue("blank");
         if(i == 0 || i ==7){
